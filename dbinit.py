3cfd9ca9ac38dd504e2cc50680eea93939fec905
Rahul Gupta <rahulgupta02@yahoo.com>
2021-09-30 23:41:05 +0530

    first-commit

---
+++ dbinit.py
@@ -0,0 +1,13 @@
+from app import db, User, Role, user_datastore, app
+from flask_security.utils import hash_password
+with app.app_context():
+
+    db.create_all()
+    r=user_datastore.create_role(name="Admin")
+    u = user_datastore.create_user(name = "admin", email="rg19961970@gmail.com", password=hash_password("admin"), roles = ['Admin'])
+    db.session.add(u)
+    db.session.add(r)
+    db.session.commit()
+    user = User.query.all()
+    print(user)         
+
